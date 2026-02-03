6368c44a90221d54026a552a608ffafa203be2ec
apenukonda <aryanpenukonda@gmail.com>
2026-02-04 02:04:42 +0530

    hero correction

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             playsInline
             className={styles.heroVideo}
           >
-            <source src="/v3_enhanced.mp4" type="video/mp4" />
+            <source src="/v3.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
